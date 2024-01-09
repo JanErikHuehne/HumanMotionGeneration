234617dda1284c99e7fe3af22435c41494b72f9d
Jchao-Xie <149434116+Jchao-Xie@users.noreply.github.com>
2024-01-09 09:43:26 +0100

    vector_representation

---
+++ s2m_vector_representation/sample/generate.py
@@ -0,0 +1,312 @@
+from utils.fixseed import fixseed
+import os
+import numpy as np
+import torch
+from utils.parser_util import generate_args
+from utils.model_util import create_model_and_diffusion, load_model_wo_clip
+from utils import dist_util
+# from model.cfg_sampler import ClassifierFreeSampleModel
+from data_loaders.dataloader import get_dataset_loader
+from data_loaders.humanml.scripts.motion_process import recover_from_ric
+import data_loaders.humanml.utils.paramUtil as paramUtil
+from data_loaders.humanml.utils.plot_script import plot_3d_motion
+import shutil
+# from data_loaders.tensors import collate
+from model.mdm import MDM
+from diffusion import Gaussian_diffusion as gd
+import torch as th
+# from model.rotation2xyz import Rotation2xyz as rot2xyz
+from model.rotation2xyz import Rotation2xyz
+
+def dev():
+    """
+    Get the device to use for torch.distributed.
+    """
+    if th.cuda.is_available():
+        print("cuda")
+        return th.device(f"cuda:0")
+    return th.device("cpu")
+
+def main():
+    args = generate_args()      # args 是用来接受输入的，如--model.path
+    #args.model_path = './save/humanml_trans_enc_512/model000200000.pt'
+    # args.text_prompt = 'the person walked forward and is picking up his toolbox.'
+    fixseed(args.seed)
+    out_path = args.output_dir
+    name = os.path.basename(os.path.dirname(args.model_path))
+    niter = os.path.basename(args.model_path).replace('model', '').replace('.pt', '')
+    max_frames = 41 if args.dataset in ['kit', 'humanml'] else 60
+    fps = 12.5 if args.dataset == 'kit' else 20
+    n_frames = min(max_frames, int(args.motion_length*fps))
+    is_using_data = not any([args.input_text, args.text_prompt, args.action_file, args.action_name])
+    # is_using_data will be True if none of the specified arguments are provided (all are False or not present), and False if at least one of these arguments is provided.
+    dist_util.setup_dist(args.device)
+    if out_path == '':
+        out_path = os.path.join(os.path.dirname(args.model_path),
+                                'samples_{}_{}_seed{}'.format(name, niter, args.seed))
+        if args.text_prompt != '':
+            out_path += '_' + args.text_prompt.replace(' ', '_').replace('.', '')
+        elif args.input_text != '':
+            out_path += '_' + os.path.basename(args.input_text).replace('.txt', '').replace(' ', '_').replace('.', '')
+
+    # this block must be called BEFORE the dataset is loaded
+    args.num_samples = 1
+    if args.text_prompt != '':
+        texts = [args.text_prompt]
+        args.num_samples = 1
+    elif args.input_text != '':
+        assert os.path.exists(args.input_text)
+        with open(args.input_text, 'r') as fr:
+            texts = fr.readlines()
+        texts = [s.replace('\n', '') for s in texts]
+        args.num_samples = len(texts)
+    elif args.action_name:
+        action_text = [args.action_name]
+        args.num_samples = 1
+    elif args.action_file != '':
+        assert os.path.exists(args.action_file)
+        with open(args.action_file, 'r') as fr:
+            action_text = fr.readlines()
+        action_text = [s.replace('\n', '') for s in action_text]
+        args.num_samples = len(action_text)
+
+    assert args.num_samples <= args.batch_size, \
+        f'Please either increase batch_size({args.batch_size}) or reduce num_samples({args.num_samples})'
+    # So why do we need this check? In order to protect GPU from a memory overload in the following line.
+    # If your GPU can handle batch size larger then default, you can specify it through --batch_size flag.
+    # If it doesn't, and you still want to sample more prompts, run this script with different seeds
+    # (specify through the --seed flag)
+    args.batch_size = args.num_samples  # Sampling a single batch from the testset, with exactly args.num_samples
+
+    print('Loading dataset...')
+    loader = get_dataset_loader(datapath='test_data/humanml_opt.txt', batch_size=1)
+    total_num_samples = args.num_samples * args.num_repetitions
+
+    print("Creating model and diffusion...")
+    model = MDM()
+    betas = gd.get_named_beta_schedule(schedule_name="cosine", num_diffusion_timesteps=1000)
+    # loss_type = gd.LossType.MSE
+    model.to(dev())
+    diffusion = gd.GaussianDiffusion(betas=betas)
+
+    print(f"Loading checkpoints from [{args.model_path}]...")
+    state_dict = torch.load(args.model_path, map_location='cpu')
+    x1 = []
+    x2 = []
+    print("Before loading state_dict:")
+    for param_tensor in model.state_dict():
+        print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+        print(model.input_process.poseEmbedding.weight.data)
+        break
+
+    # Load state_dict
+    missing_keys, unexpected_keys = model.load_state_dict(state_dict, strict=False)
+    assert len(unexpected_keys) == 0
+    # assert all([k.startswith('clip_model.') for k in missing_keys])
+    # model.load_state_dict(state_dict)
+
+    # After loading
+    print("\nAfter loading state_dict:")
+
+    for param_tensor in model.state_dict():
+        print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+        print(model.input_process.poseEmbedding.weight.data)
+        break
+
+    # load_model_wo_clip(model, state_dict)
+
+    # if args.guidance_param != 1:
+    #     model = ClassifierFreeSampleModel(model)   # wrapping model with the classifier-free sampler
+    # model.to(dist_util.dev())
+    model.eval()  # disable random masking
+
+    # if is_using_data:
+    #     iterator = iter(data)
+    #     _, model_kwargs = next(iterator)
+    # else:
+    #     collate_args = [{'inp': torch.zeros(n_frames), 'tokens': None, 'lengths': n_frames}] * args.num_samples
+    #     is_t2m = any([args.input_text, args.text_prompt])
+    #     if is_t2m:
+    #         # t2m
+    #         collate_args = [dict(arg, text=txt) for arg, txt in zip(collate_args, texts)]
+    #     else:
+    #         # a2m
+    #         action = data.dataset.action_name_to_action(action_text)
+    #         collate_args = [dict(arg, action=one_action, action_text=one_action_text) for
+    #                         arg, one_action, one_action_text in zip(collate_args, action, action_text)]
+    #     _, model_kwargs = collate(collate_args)
+
+    all_motions = []
+    all_lengths = []
+    all_text = []
+
+    for rep_i in range(args.num_repetitions):
+    # for rep_i, (motion, sketch, keyframe) in enumerate(loader):
+        print(f'### Sampling [repetitions #{rep_i}]')
+        sketch = None
+        motion = None
+        keyframe = None
+        ####################################
+
+        for i, (motion1, sketch1, keyframe1) in enumerate(loader):
+            if i == 10:
+        # add CFG scale to batch
+        # if args.guidance_param != 1:
+        #     model_kwargs['y']['scale'] = torch.ones(args.batch_size, device=dist_util.dev()) * args.guidance_param
+        # sketch = torch.zeros_like(sketch)
+                # sketch.to(dev())
+                # sketch['pixel_values'] = torch.zeros_like(sketch['pixel_values'])
+                motion = motion1
+                sketch = sketch1
+                keyframe = keyframe1
+                # keyframe = torch.zeros_like(keyframe)
+                break
+
+        #####################################
+        sample_fn = diffusion.p_sample_loop
+
+        sample = sample_fn(
+            model,
+            # (args.batch_size, model.njoints, model.nfeats, n_frames),  # BUG FIX - this one caused a mismatch between training and inference
+            (args.batch_size, model.njoints, model.nfeats, max_frames),  # BUG FIX
+            clip_denoised=False,
+            sketch=sketch,
+            keyframe=keyframe,
+            skip_timesteps=0,  # 0 is the default value - i.e. don't skip any step
+            init_image=None,
+            progress=True,
+            dump_steps=None,
+            noise=None,
+            const_noise=False,
+        )
+
+        # Recover XYZ *positions* from HumanML3D vector representation
+        ######
+        # # sample = motion
+        # motion = torch.unsqueeze(motion, dim=0).permute(0, 3, 1, 2)
+        # sample = motion
+        # ######
+        n_joints = 22
+        sample = loader.dataset.t2m_dataset.inverse_norm(sample.cpu().permute(0, 2, 3, 1)).float()
+        sample = recover_from_ric(sample, n_joints)
+        sample = sample.view(-1, *sample.shape[2:]).permute(0, 2, 3, 1)
+
+        rot2xyz_pose_rep = 'xyz'
+        rot2xyz_mask = None
+        sample = model.rot2xyz(x=sample, mask=rot2xyz_mask, pose_rep=rot2xyz_pose_rep, glob=True, translation=True,
+                               jointstype='smpl', vertstrans=True, betas=None, beta=0, glob_rot=None,
+                               get_rotations_back=False)
+        all_text.append(str(rep_i))
+        all_motions.append(sample.cpu().numpy())
+        all_lengths.append(np.array([motion.shape[1]]))
+
+        print(f"created {len(all_motions) * args.batch_size} samples")
+
+    all_lengths = np.array(all_lengths)
+    all_motions = np.concatenate(all_motions, axis=0)
+    all_motions = all_motions[:total_num_samples]  # [bs, njoints, 6, seqlen]
+    all_text = all_text[:total_num_samples]
+    all_lengths = np.concatenate(all_lengths, axis=0)[:total_num_samples]
+
+    if os.path.exists(out_path):
+        shutil.rmtree(out_path)
+    os.makedirs(out_path)
+
+    npy_path = os.path.join(out_path, 'results.npy')
+    print(f"saving results file to [{npy_path}]")
+    np.save(npy_path,
+            {'motion': all_motions, 'text': all_text, 'lengths': all_lengths,
+             'num_samples': args.num_samples, 'num_repetitions': args.num_repetitions})
+    with open(npy_path.replace('.npy', '.txt'), 'w') as fw:
+        fw.write('\n'.join(all_text))
+    with open(npy_path.replace('.npy', '_len.txt'), 'w') as fw:
+        fw.write('\n'.join([str(l) for l in all_lengths]))
+
+    print(f"saving visualizations to [{out_path}]...")
+    skeleton = paramUtil.kit_kinematic_chain if args.dataset == 'kit' else paramUtil.t2m_kinematic_chain
+
+    sample_files = []
+    num_samples_in_out_file = 7
+
+    sample_print_template, row_print_template, all_print_template, \
+        sample_file_template, row_file_template, all_file_template = construct_template_variables(
+        args.unconstrained)
+
+    for sample_i in range(args.num_samples):
+        rep_files = []
+        for rep_i in range(args.num_repetitions):
+            caption = all_text[rep_i * args.batch_size + sample_i]
+            length = all_lengths[rep_i * args.batch_size + sample_i]
+            motion = all_motions[rep_i * args.batch_size + sample_i].transpose(2, 0, 1)[:length]
+            save_file = sample_file_template.format(sample_i, rep_i)
+            print(sample_print_template.format(caption, sample_i, rep_i, save_file))
+            animation_save_path = os.path.join(out_path, save_file)
+            plot_3d_motion(animation_save_path, skeleton, motion, dataset=args.dataset, title=caption, fps=fps)
+            # Credit for visualization: https://github.com/EricGuo5513/text-to-motion
+            rep_files.append(animation_save_path)
+
+        sample_files = save_multiple_samples(args, out_path,
+                                             row_print_template, all_print_template, row_file_template,
+                                             all_file_template,
+                                             caption, num_samples_in_out_file, rep_files, sample_files, sample_i)
+
+    abs_path = os.path.abspath(out_path)
+    print(f'[Done] Results are at [{abs_path}]')
+
+def save_multiple_samples(args, out_path, row_print_template, all_print_template, row_file_template, all_file_template,
+                          caption, num_samples_in_out_file, rep_files, sample_files, sample_i):
+    all_rep_save_file = row_file_template.format(sample_i)
+    all_rep_save_path = os.path.join(out_path, all_rep_save_file)
+    ffmpeg_rep_files = [f' -i {f} ' for f in rep_files]
+    hstack_args = f' -filter_complex hstack=inputs={args.num_repetitions}' if args.num_repetitions > 1 else ''
+    ffmpeg_rep_cmd = f'ffmpeg -y -loglevel warning ' + ''.join(ffmpeg_rep_files) + f'{hstack_args} {all_rep_save_path}'
+    os.system(ffmpeg_rep_cmd)
+    print(row_print_template.format(caption, sample_i, all_rep_save_file))
+    sample_files.append(all_rep_save_path)
+    if (sample_i + 1) % num_samples_in_out_file == 0 or sample_i + 1 == args.num_samples:
+        # all_sample_save_file =  f'samples_{(sample_i - len(sample_files) + 1):02d}_to_{sample_i:02d}.mp4'
+        all_sample_save_file = all_file_template.format(sample_i - len(sample_files) + 1, sample_i)
+        all_sample_save_path = os.path.join(out_path, all_sample_save_file)
+        print(all_print_template.format(sample_i - len(sample_files) + 1, sample_i, all_sample_save_file))
+        ffmpeg_rep_files = [f' -i {f} ' for f in sample_files]
+        vstack_args = f' -filter_complex vstack=inputs={len(sample_files)}' if len(sample_files) > 1 else ''
+        ffmpeg_rep_cmd = f'ffmpeg -y -loglevel warning ' + ''.join(
+            ffmpeg_rep_files) + f'{vstack_args} {all_sample_save_path}'
+        os.system(ffmpeg_rep_cmd)
+        sample_files = []
+    return sample_files
+
+
+def construct_template_variables(unconstrained):
+    row_file_template = 'sample{:02d}.mp4'
+    all_file_template = 'samples_{:02d}_to_{:02d}.mp4'
+    if unconstrained:
+        sample_file_template = 'row{:02d}_col{:02d}.mp4'
+        sample_print_template = '[{} row #{:02d} column #{:02d} | -> {}]'
+        row_file_template = row_file_template.replace('sample', 'row')
+        row_print_template = '[{} row #{:02d} | all columns | -> {}]'
+        all_file_template = all_file_template.replace('samples', 'rows')
+        all_print_template = '[rows {:02d} to {:02d} | -> {}]'
+    else:
+        sample_file_template = 'sample{:02d}_rep{:02d}.mp4'
+        sample_print_template = '["{}" ({:02d}) | Rep #{:02d} | -> {}]'
+        row_print_template = '[ "{}" ({:02d}) | all repetitions | -> {}]'
+        all_print_template = '[samples {:02d} to {:02d} | all repetitions | -> {}]'
+
+    return sample_print_template, row_print_template, all_print_template, \
+           sample_file_template, row_file_template, all_file_template
+
+
+# def load_dataset(args, max_frames, n_frames):
+#     data = get_dataset_loader(name=args.dataset,
+#                               batch_size=args.batch_size,
+#                               num_frames=max_frames,
+#                               split='test',
+#                               hml_mode='text_only')
+#     if args.dataset in ['kit', 'humanml']:
+#         data.dataset.t2m_dataset.fixed_length = n_frames
+#     return data
+
+
+if __name__ == "__main__":
+    main()
